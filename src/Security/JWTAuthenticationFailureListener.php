87f143b4fe77033d6f819ed6d84fe4abadf53219
Alessandro Bellanda <a.bellanda@esconsulting.it>
2020-06-15 12:03:16 +0200

    refactor: added log when auth fails

--- src/Security/JWTAuthenticationFailureListener.php
+++ src/Security/JWTAuthenticationFailureListener.php
@@ -4,12 +4,25 @@ namespace Esc\JwtAuth\Security;
 
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationFailureEvent;
 use Lexik\Bundle\JWTAuthenticationBundle\Response\JWTAuthenticationFailureResponse;
+use Psr\Log\LoggerInterface;
 
 class JWTAuthenticationFailureListener
 {
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function onAuthenticationFailureResponse(AuthenticationFailureEvent $event): void
     {
         $response = new JWTAuthenticationFailureResponse('Utente e/o Passowrd non validi');
+
+        $this->logger->error($event->getException()->getMessage());
 
         $event->setResponse($response);
     }
